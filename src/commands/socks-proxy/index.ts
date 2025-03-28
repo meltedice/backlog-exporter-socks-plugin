47cbf5be14e6f4dd1d278f4e3c4ce64d51da6203
ice <meltedise@gmail.com>
2025-03-28 23:39:39 +0900

    add description to socks-proxy command

--- src/commands/socks-proxy/index.ts
+++ src/commands/socks-proxy/index.ts
@@ -16,7 +16,14 @@ export default class SocksProxy extends Command {
   async run(): Promise<void> {
     this.log(`To use the socks proxy, you need to set the environment variable SOCKS_PROXY to the address of the socks proxy.`)
     this.log(`For example:`)
-    this.log(`export SOCKS_PROXY=socks5://example.com:1080`)
+    this.log(`  export SOCKS_PROXY=socks5://example.com:1080`)
     this.log(`This will tell the system to use the socks proxy at example.com:1080 to connect to the internet.`)
+    this.log('')
+    this.log('Your current SOCKS_PROXY environment variable is:', process.env.SOCKS_PROXY)
+    if (process.env.SOCKS_PROXY?.startsWith('socks')) {
+      this.log('You will call all Backlog APIs through the socks proxy.')
+    } else {
+      this.log('You will not call all Backlog APIs through the socks proxy.')
+    }
   }
 }
